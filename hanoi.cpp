7400c98516f2d8204234b1c2a801ff29b5daf2b3
svlalli <157869493+svlalli@users.noreply.github.com>
2024-02-18 11:23:57 +0530

    Add files via upload

---
+++ hanoi.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void towerOfHanoi(int n, char source, char auxiliary, char destination) {
+    if (n == 1) {
+        printf("Move disk 1 from %c to %c\n", source, destination);
+        return;
+    }
+    towerOfHanoi(n - 1, source, destination, auxiliary);
+    printf("Move disk %d from %c to %c\n", n, source, destination);
+    towerOfHanoi(n - 1, auxiliary, source, destination);
+}
+
+int main() {
+    int numDisks;
+
+    printf("Enter the number of disks: ");
+    scanf("%d", &numDisks);
+
+    printf("Tower of Hanoi solution:\n");
+    towerOfHanoi(numDisks, 'A', 'B', 'C');
+
+    return 0;
+}
